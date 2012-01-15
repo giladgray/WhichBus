b0e1d4e6d92f77ce347133cf1e907d607b6a46d9
Gilad Gray <giladgray@gmail.com>
2012-01-14 22:41:35 -0800

    journey stuff

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -1,3 +1,5 @@
+require 'stop'
+
 class JourneyController < ApplicationController
 	include GeoKit::Geocoders
 
@@ -5,15 +7,20 @@ class JourneyController < ApplicationController
 	end
 	
 	def options
+		#expects two locations -- geocode them straight up!
 		@from = GoogleGeocoder.geocode(params[:from])
 		@to = GoogleGeocoder.geocode(params[:to])
 		
 		#if(@from.success and @to.success)
-		#expects two locations
-		#geocode both to lat/lng
+			#error handling!
+		
 		#find stops around those locations
+		@from_stops = Stop.by_location(@from.lat, @from.lng).first(10)
+		@to_stops = Stop.by_location(@to.lat, @to.lng).first(10)
 		#call routing helper to find the routes
+		
 		#display them
+		
 	end
 	
 	def show
