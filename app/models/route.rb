431b94232e4b5e6f85d1dc66136407e1b888f724
Dave Remy <email@daveremy.com>
2012-01-14 22:34:51 -0800

    journey helper, changes to route.rb

--- app/models/route.rb
+++ app/models/route.rb
@@ -10,5 +10,11 @@ class Route < OneBusRecord
    url = "http://api.onebusaway.org/api/where/route/#{route}.json?key=TEST"
    hash ? super(hash) : super(url)
   end
+  
+  def stops
+	url = "http://api.onebusaway.org/api/where/stops-for-route/#{data.id}.json?key=TEST&version=2"
+	puts url
+	self.class.get_json(url)["data"]["entry"]["stopIds"]
+  end
 
 end
