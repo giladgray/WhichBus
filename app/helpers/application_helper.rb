7ac41050ccb72d99c384d613681ef0ede46dc690
Gilad Gray <giladgray@gmail.com>
2012-01-23 23:51:47 -0800

    added application helpers, fixed up universal layout so there's only one map element (still needs work on mobile), moved some javascript around

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+	def route_button(route)
+		link_to(route.shortName, route_path(route.id), :class=>"button radius")
+	end
+	
+	def predicted_time(arrival)
+		# if we have a real prediction....
+		if arrival.predictedDepartureTime > 0
+			html = ""
+			html << content_tag(:div, arrival.predicted_departure_time, :class=>"row small")
+			html << content_tag(:div, arrival.time_to_departure_in_words(@time), :class=>"row #{arrival.css_class_for_arrival_time(@time)}")
+			html << content_tag(:div, arrival.predicted_departure_difference, :class=>"row small #{arrival.css_class_for_time_difference}")
+			# must call this method for the HTML to appear properly on the page
+			html.html_safe
+		# otherwise use scheduled time
+		else
+			scheduled_time(arrival)
+		end
+	end
+	
+	def scheduled_time(arrival)
+		html = ""
+		html << content_tag(:div, arrival.scheduled_arrival_time, :class=>"row small")
+		html << content_tag(:div, arrival.time_to_arrival_in_words(@time), :class=>"row #{arrival.css_class_for_arrival_time(@time)}")
+		html << content_tag(:div, "scheduled", :class=>"row small")
+		html.html_safe
+	end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,3 +9,39 @@ function toggleHidden() {
 		btn.text("Show Boring Routes");
 	$(".row.journey.hidden").toggle();
 }
+
+
+/* Google Maps JS */
+function geolocate() {
+	navigator.geolocation.getCurrentPosition(initializeMap, noLocation);
+}
+
+function noLocation() {
+	alert("Geolocation failed!");
+}
+
+function initializeMap(position) {
+
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	var myLatlng = new google.maps.LatLng(latitude,longitude);
+	
+  var myOptions = {
+    zoom: 14,
+    center: new google.maps.LatLng(latitude, longitude),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    minzoom: 10,
+    disableDefaultUI: true
+  }
+
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title:"Your Location"
+  });
+}
+
+window.onload = geolocate;
+/* Google Maps JS END */
