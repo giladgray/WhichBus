c27bdc0369706d749acb9870854ec3a151ec336a
Gilad Gray <giladgray@gmail.com>
2012-04-28 00:52:52 -0700

    high time for an upgrade. WhichBus now runs Rails 3.2!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,5 +31,12 @@ Whichbus::Application.configure do
   # Enable caching
   config.cache_store = :memory_store
   config.action_controller.perform_caching = false
+
+	# Raise exception on mass assignment protection for Active Record models
+	config.active_record.mass_assignment_sanitizer = :strict
+
+	# Log the query plan for queries taking more than this (works
+	# with SQLite, MySQL, and PostgreSQL)
+	config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
 
